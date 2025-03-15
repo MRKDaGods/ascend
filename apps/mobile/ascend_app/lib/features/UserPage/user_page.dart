389ce33c8d47920f8cd560b6721777cea1d64ffd
MagedWadi <maged.wadi02@eng-st.cu.edu.eg>
2025-03-15 16:27:38 +0200

    Update user_page.dart

--- apps/mobile/ascend_app/lib/features/UserPage/user_page.dart
+++ apps/mobile/ascend_app/lib/features/UserPage/user_page.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
+
 class UserProfilePage extends StatelessWidget {
   const UserProfilePage(this.changescreen, {super.key});
   final void Function(int) changescreen;
