0684fc9de255d40966b4032b6b6de5593a4cbb7c
rtolba04 <153285658+rtolba04@users.noreply.github.com>
2025-03-16 05:46:33 +0200

    static sidebar with two issues

--- apps/web/template/src/app/page.tsx
+++ apps/web/template/src/app/page.tsx
@@ -1,10 +1,15 @@
 import styles from "./page.module.css";
+import Link from 'next/link';
+
 
 function Home() {
   return (
     <div className={styles.page}>
       <p>Integrate here, guyss!</p>
-      <p>hiii</p>
+      <Link href="/chat">
+                <button>Go to Chat</button>
+            </Link>
+      
     </div>
   );
 }
