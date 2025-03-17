7b4165fc4e3467820f8bcb1508229ce9fdf1037c
Hana Ahmed <hanaahmedghanem@gmail.com>
2025-03-17 22:45:01 +0200

    trial

--- apps/web/template/src/app/page.tsx
+++ apps/web/template/src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 function Home() {
   return (
     <div className={styles.page}>
-      <p>Integrate here, guys!</p>
+      <p>Integrate here</p>
     </div>
   );
 }
