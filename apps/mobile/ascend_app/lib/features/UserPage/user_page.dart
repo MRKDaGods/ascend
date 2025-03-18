ec75c3a0fdf8ffc08a2211a3e2c857cc2ec7ab58
MagedWadi <maged.wadi02@eng-st.cu.edu.eg>
2025-03-19 01:35:18 +0200

    phase1

--- apps/mobile/ascend_app/lib/features/UserPage/user_page.dart
+++ apps/mobile/ascend_app/lib/features/UserPage/user_page.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
+import 'models/profile_section.dart';
+import 'package:ascend_app/features/home/presentation/pages/home.dart';
+import 'package:ascend_app/shared/widgets/custom_sliver_appbar.dart';
+import 'full_screen_image.dart';
 
 class UserProfilePage extends StatelessWidget {
-  const UserProfilePage(this.changescreen, {super.key});
-  final void Function(int) changescreen;
+  UserProfilePage({
+    super.key,
+    this.name = 'Hamada Helal',
+    this.bio = "zzz",
+    this.profileImageUrl = 'https://picsum.photos/150/150',
+    this.coverImageUrl = 'https://picsum.photos/1500/500',
+    this.location = 'Cairo, Cairo, Egypt',
+    this.latestEducation = 'Cairo University',
+    this.sections = const [],
+    this.isconnect = false,
+    this.isfollow = false,
+    this.connections = 15,
+  });
+
+  final String name;
+  final bool isconnect;
+  final bool isfollow;
+  final int connections;
+  final String latestEducation;
+  final String bio;
+  final String profileImageUrl;
+  final String coverImageUrl;
+  final String location;
+  List<ProfileSection> sections;
+  void _showFullScreenImage(BuildContext context, String imageUrl) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FullScreenImage(imageUrl: imageUrl),
+      ),
+    );
+  }
+
+  //final void Function(int) changescreen;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,8 +48,11 @@ class UserProfilePage extends StatelessWidget {
         elevation: 0,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () => changescreen(0),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
+        //search bar
         title: Container(
           height: 40,
           decoration: BoxDecoration(
@@ -21,12 +60,15 @@ class UserProfilePage extends StatelessWidget {
             borderRadius: BorderRadius.circular(8),
           ),
           child: TextField(
-            style: TextStyle(color: Colors.white),
+            style: TextStyle(color: const Color.fromARGB(255, 19, 19, 19)),
             decoration: InputDecoration(
-              prefixIcon: Icon(Icons.search, color: Colors.white54),
+              prefixIcon: Icon(
+                Icons.search,
+                color: const Color.fromARGB(137, 0, 0, 0),
+              ),
               border: InputBorder.none,
               hintText: 'Search',
-              hintStyle: TextStyle(color: Colors.white54),
+              hintStyle: TextStyle(color: const Color.fromARGB(137, 3, 3, 3)),
               suffixStyle: TextStyle(color: Colors.white54),
             ),
           ),
@@ -39,23 +81,40 @@ class UserProfilePage extends StatelessWidget {
               clipBehavior: Clip.none,
               alignment: Alignment.bottomLeft,
               children: [
-                Container(height: 120, color: Colors.grey[800]),
+                GestureDetector(
+                  onTap: () {
+                    _showFullScreenImage(context, coverImageUrl);
+                  },
+                  child: Container(
+                    height: 120,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(coverImageUrl),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
                 Positioned(
                   left: 20,
                   bottom: -40,
-                  child: CircleAvatar(
-                    radius: 40,
-                    backgroundColor: Colors.black,
+                  child: GestureDetector(
+                    onTap: () {
+                      _showFullScreenImage(context, profileImageUrl);
+                    },
                     child: CircleAvatar(
-                      radius: 38,
-                      backgroundImage: NetworkImage(
-                        'https://via.placeholder.com/150',
+                      radius: 50,
+                      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundImage: NetworkImage(profileImageUrl),
                       ),
                     ),
                   ),
                 ),
               ],
             ),
+
             SizedBox(height: 50),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 20),
@@ -63,7 +122,7 @@ class UserProfilePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Hamada Helal',
+                    name,
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 22,
@@ -71,111 +130,100 @@ class UserProfilePage extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 5),
-                  Text(
-                    'Student at Cairo University',
-                    style: TextStyle(color: Colors.white70),
-                  ),
+                  Text(bio, style: TextStyle(color: Colors.white70)),
                   SizedBox(height: 5),
                   Text(
-                    'Cairo University\nCairo, Cairo, Egypt',
+                    latestEducation,
                     style: TextStyle(color: Colors.white70),
                   ),
-                  SizedBox(height: 10),
+                  Text(location, style: TextStyle(color: Colors.white70)),
+                  SizedBox(height: 5),
+                  if (connections > 0)
+                    Text(
+                      connections < 500
+                          ? '$connections connections'
+                          : '500+ connections',
+                      style: TextStyle(color: Colors.white70),
+                    ),
+
+                  SizedBox(height: 15),
                   Row(
-                    children: [
-                      ElevatedButton.icon(
-                        label: Text('Connect'),
-                        icon: Icon(Icons.add),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                        ),
-                        onPressed: () {},
-                      ),
-                      SizedBox(width: 10),
-                      OutlinedButton(
-                        onPressed: () {},
-                        style: OutlinedButton.styleFrom(
-                          side: BorderSide(color: Colors.white70),
-                        ),
-                        child: Text(
-                          'Message',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ],
+                    children:
+                        isconnect
+                            ? [
+                              // Wide "Message" Button when connected
+                              Expanded(
+                                child: OutlinedButton(
+                                  onPressed: () {},
+                                  style: OutlinedButton.styleFrom(
+                                    side: BorderSide(color: Colors.white70),
+                                    padding: EdgeInsets.symmetric(
+                                      vertical: 15,
+                                    ), // Make it taller
+                                  ),
+                                  child: Text(
+                                    'Message',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ]
+                            : [
+                              // "Connect" and "Message" buttons when NOT connected
+                              ElevatedButton.icon(
+                                label: Text('Connect'),
+                                icon: Icon(Icons.add),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blue,
+                                ),
+                                onPressed: () {},
+                              ),
+                              SizedBox(width: 10),
+                              OutlinedButton(
+                                onPressed: () {},
+                                style: OutlinedButton.styleFrom(
+                                  side: BorderSide(color: Colors.white70),
+                                ),
+                                child: Text(
+                                  'Message',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ],
                   ),
+
                   SizedBox(height: 30),
                   // Divider(color: Colors.white38),
-                  _buildSection('Highlights', [
-                    'You both studied at Cairo University from 2021 to 2026',
-                  ]),
-                  _buildSection('Activity', ['Hamada hasn’t posted yet']),
-                  _buildEducation(),
-                  _buildInterests(),
+                  for (var section in sections) _buildSection(section),
                 ],
               ),
             ),
           ],
         ),
       ),
-      bottomNavigationBar: _buildnNavigationBar(),
     );
   }
 
-  Widget _buildSection(String title, List<String> content) {
+  Widget _buildSection(ProfileSection section) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          title,
+          section.title,
           style: TextStyle(
             color: Colors.white,
             fontSize: 18,
             fontWeight: FontWeight.bold,
           ),
         ),
-        for (var item in content) ...[
+        for (var item in section.content) ...[
           SizedBox(height: 5),
-          Text(item, style: TextStyle(color: Colors.white70)),
+          item,
           Divider(color: Colors.white38),
         ],
         SizedBox(height: 20),
         // Divider(color: Colors.white38),
       ],
     );
-  }
-
-  Widget _buildEducation() {
-    return _buildSection('Education', [
-      'Cairo University\nBachelor of Engineering - Computer Engineering\nOct 2021 - Jun 2026',
-      'Pioneers Language School\nHigh School\nSep 2018 - Jun 2021',
-    ]);
-  }
-
-  Widget _buildnNavigationBar() {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: Colors.black,
-      selectedItemColor: Colors.blue,
-      unselectedItemColor: Colors.white60,
-      items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.people), label: 'My Network'),
-        BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Post'),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.notifications),
-          label: 'Notifications',
-        ),
-        BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Jobs'),
-      ],
-    );
-  }
-
-  Widget _buildInterests() {
-    return _buildSection('Interests', [
-      'Cairo University',
-      'Microsoft',
-      'Flutter',
-    ]);
   }
 }
--- apps/mobile/ascend_app/lib/shared/navigation/main_navigation.dart
+++ apps/mobile/ascend_app/lib/shared/navigation/main_navigation.dart
@@ -1,6 +1,53 @@
+import 'package:ascend_app/features/UserPage/user_page.dart';
 import 'package:flutter/material.dart';
 import 'package:ascend_app/features/home/presentation/pages/home.dart';
 import 'package:ascend_app/features/networks/presentation/networks.dart';
+import 'package:ascend_app/features/UserPage/models/profile_section.dart';
+
+final List<ProfileSection> sectionss = [
+  ProfileSection(
+    title: 'Highlights',
+    content: [
+      Text(
+        'You both studied at Cairo University from 2021 to 2026',
+        style: TextStyle(color: Colors.white, fontSize: 14),
+      ),
+    ],
+  ),
+  ProfileSection(
+    title: 'Activity',
+    content: [
+      Text(
+        'Hamada hasn’t posted yet',
+        style: TextStyle(color: Colors.white, fontSize: 14),
+      ),
+    ],
+  ),
+  ProfileSection(
+    title: 'Education',
+    content: [
+      Text(
+        'Cairo University\nBachelor of Engineering - Computer Engineering\nOct 2021 - Jun 2026',
+        style: TextStyle(color: Colors.white, fontSize: 14),
+      ),
+      Text(
+        'Pioneers Language School\nHigh School\nSep 20 14 - Jun 2021',
+        style: TextStyle(color: Colors.white, fontSize: 14),
+      ),
+    ],
+  ),
+  ProfileSection(
+    title: 'Interests',
+    content: [
+      Text(
+        'Cairo University',
+        style: TextStyle(color: Colors.white, fontSize: 14),
+      ),
+      Text('Microsoft', style: TextStyle(color: Colors.white, fontSize: 14)),
+      Text('Flutter', style: TextStyle(color: Colors.white, fontSize: 14)),
+    ],
+  ),
+];
 
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
@@ -11,8 +58,15 @@ class MainNavigation extends StatefulWidget {
 
 class _MainNavigationState extends State<MainNavigation> {
   int _selectedIndex = 0;
-  
-  final List<Widget> _pages = const [
+
+  final List<Widget> _pages = [
+    UserProfilePage(
+      name: "Maged Amgad",
+      bio: "sw",
+      location: "Cairo,Egypt",
+      latestEducation: "Cairo university",
+      sections: sectionss,
+    ),
     Home(),
     Center(child: Text("Video")),
     Networks(),
@@ -29,35 +83,23 @@ class _MainNavigationState extends State<MainNavigation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _pages,
-      ),
+      body: IndexedStack(index: _selectedIndex, children: _pages),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         type: BottomNavigationBarType.fixed,
         onTap: _onItemTapped,
         items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(
             icon: Icon(Icons.video_library),
             label: 'Video',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.groups),
-            label: 'Networks',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.groups), label: 'Networks'),
           BottomNavigationBarItem(
             icon: Icon(Icons.notifications),
             label: 'Notifications',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.work),
-            label: 'Jobs',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Jobs'),
         ],
       ),
     );
