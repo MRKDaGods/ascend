cedcede868716fd29df64b2b32262b783eb5c11a
rtolba04 <153285658+rtolba04@users.noreply.github.com>
2025-03-20 10:41:13 +0200

    dynamic sidebar and zustand chatstore (UI responsiveness bayez)

--- apps/web/template/src/app/chat/components/ChatWindow.tsx
+++ apps/web/template/src/app/chat/components/ChatWindow.tsx
@@ -1,4 +1,13 @@
 "use client";
+import { Box,Typography} from "@mui/material";
 
+export default function ChatWindow(){
+    return(
+        <Box sx={{flexGrow: 1, height: "calc(100vh - 64px)", overflowY: "auto" }}>
+        <Typography variant="h5" sx={{color:"#495057" }}>select conversation to start chatting</Typography>
 
+    </Box>
+    );
+   
+}
 
