76bc7bc098cf9c77434f846156b69566ca9c7569
rtolba04 <153285658+rtolba04@users.noreply.github.com>
2025-03-25 23:34:13 +0200

    api call for messages and pagination

--- apps/web/template/src/app/chat/components/ChatWindow.tsx
+++ apps/web/template/src/app/chat/components/ChatWindow.tsx
@@ -1,13 +1,85 @@
 "use client";
-import { Box,Typography} from "@mui/material";
+import { Box,Typography,Button} from "@mui/material";
+import { useEffect} from "react";
+import { useChatStore } from "../store/chatStore";
+import axios from "axios";
 
 export default function ChatWindow(){
-    return(
-        <Box sx={{flexGrow: 1, height: "calc(100vh - 64px)", overflowY: "auto" }}>
-        <Typography variant="h5" sx={{color:"#495057" }}>select conversation to start chatting</Typography>
 
-    </Box>
-    );
+    
+    //worse readablioty compared to destructuring bas ahsan in avoiding unnecessary rerendering of store
+    const selectedConversationId = useChatStore(state => state.selectedConversationId);
+    const setDisplayedMessages = useChatStore(state => state.setDisplayedMessages);
+    const displayedMessages = useChatStore(state => state.displayedMessages);
+    const resetPage = useChatStore(state => state.resetPage);
+    const page= useChatStore(state=>state.page);
+    const setpage = useChatStore(state=>state.setPage);
+
+    
+  
+
+    useEffect(()=>{
+        if (!selectedConversationId) return;
+        
+        //clear old pagination
+        resetPage();
+
+        axios.get(`http://localhost:3001/messages/${selectedConversationId}?limit=20&page=1`)
+        .then((response)=>{
+            setDisplayedMessages(response.data);
+        })
+        .catch((e)=>{
+            console.error("failed to fetch messages:",e);
+        });
+    },[selectedConversationId]);
+
+    const loadOlderMessages = ()=>{
+        const nextPage=page+1;
+        axios.get(`http://localhost:3001/messages/${selectedConversationId}?limit=20&page=${nextPage}`)
+        .then((res)=>{
+            if (!Array.isArray(res.data) || 
+            res.data.length===0 ||
+            res.data.every((msg) => displayedMessages.some((m) => m.id === msg.id))
+        ) {
+        
+                console.log("no more messages to load");
+                return;
+            }
+            //else prepend older msgs to the top
+            setDisplayedMessages((prev)=>[...res.data,...prev]);
+            setpage(nextPage);
+        })
+        .catch((e)=>console.error("Failed to load older messages:", e));
+    };
+     
+
+    if (!selectedConversationId){
+        {
+            return(
+                <Box sx={{flexGrow: 1, display: "flex", alignItems:"center", justifyContent:"center" }}>
+                    <Typography variant="h6" sx={{color:"#495057" }}>
+                        select conversation to start chatting
+                    </Typography>
+             </Box>
+            );
+            
+        }
+    }
+
+   return(
+    <Box sx={{ flexGrow: 1, overflowY: "auto", padding: 2 }}>
+    <Button variant="outlined" onClick={loadOlderMessages} size="small" sx={{mb: 2}}>
+        Load older messages
+    </Button>
+    {displayedMessages.map((msg) => (
+      msg.content ? (
+        <Typography key={msg.id}>{msg.content}</Typography>
+      ) : null
+    ))}
+  </Box>
+  
+   );
+    
    
 }
 
--- apps/web/template/src/app/chat/store/chatStore.ts
+++ apps/web/template/src/app/chat/store/chatStore.ts
@@ -1,23 +1,52 @@
 import {create} from "zustand";
 import { conversation } from "../components/Sidebar";
 
+
+type message ={
+    id: number;
+    sender: string;
+    content:string;
+    media: null | {
+        type: "image" | "video" | "document";
+        url: string;
+        fileName?: string;
+    };
+    timestamp: string;
+}
+
 //should include all global states related to chat
 
-//defining the whole state (el state 3ala baadaha gowaha selectedid w conversations)
+//defining the whole state (el state 3ala baadaha gowaha selectedid w conversations w msgs)
 type chatStore ={
     selectedConversationId: number | null;
     setSelectedConversationId: (id: number | null) => void;
     conversations: conversation[],
     setConversations: (convos: conversation[])=>void;
-
+    displayedMessages: message[];
+    //momken msgs tekoon fn betakhod el previous msg array w t return a new one (updater fn)
+    setDisplayedMessages: (msgs: message[] | ((prev: message[])=>message[]))=>void;
+    page: number;
+    setPage: (newPage: number)=>void;
+    resetPage: ()=>void;
 }
 
-//actual store el lama ba call useChateStore byraga3ly the state object passed to set 
+//actual store el lama ba call useChateStore byraga3ly the state object passed to set
+//ana lama ba setWTV i update the state object bel object el b3taha l set 
 export const useChatStore = create<chatStore>((set)=>({
     selectedConversationId: null,
     setSelectedConversationId: (id)=>set({selectedConversationId: id}),
     conversations:[],
-    setConversations: (convos)=>set({conversations: convos})
+    setConversations: (convos)=>set({conversations: convos}),
+    displayedMessages:[],
+    //akeny ba2ool take current displayedmessages, pass it to msgs fn and update it with the return value of msgs
+    setDisplayedMessages: (msgs)=> 
+        typeof msgs === "function"
+             ? set((state)=>({displayedMessages:msgs(state.displayedMessages)})) 
+             : set({displayedMessages:msgs}),
+    page: 1,
+    setPage: (newPage)=>set({page:newPage}),
+    resetPage: ()=>set({page:1})
+
 
 }));
 
