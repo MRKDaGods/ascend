4c1a3c84c8b44dbfc068859dc1ff59a82d389f86
MagedWadi <maged.wadi02@eng-st.cu.edu.eg>
2025-03-29 15:04:40 +0200

    added My profile view and edit section options and page

--- apps/mobile/ascend_app/lib/features/UserPage/Data/dummy_profile_sections.dart
+++ apps/mobile/ascend_app/lib/features/UserPage/Data/dummy_profile_sections.dart
@@ -1,5 +1,6 @@
 import '../models/profile_section.dart';
 import '../profile_entry.dart';
+import 'package:flutter/material.dart';
 
 List<ProfileSection> sections = [
   // About Section
@@ -14,6 +15,28 @@ List<ProfileSection> sections = [
       ),
     ],
   ),
+  if (true)
+    ProfileSection(
+      title: 'Analytics',
+      content: [
+        ProfileEntryWidget(
+          title: '5 profile views',
+          subtitle: 'Discover who\'s viewed your profile.',
+          icon: Icon(Icons.people), // Use an appropriate icon
+        ),
+        ProfileEntryWidget(
+          title: '0 post impressions',
+          subtitle: 'Start a post to increase engagement.',
+          description: "Past 7 days",
+          icon: Icon(Icons.bar_chart), // Use an appropriate icon
+        ),
+        ProfileEntryWidget(
+          title: '8 search appearances',
+          subtitle: 'See how often you appear in search results.',
+          icon: Icon(Icons.search), // Use an appropriate icon,
+        ),
+      ],
+    ),
 
   // Education Section
   ProfileSection(
