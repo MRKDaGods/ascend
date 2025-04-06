ba18d60e324e47846628f1f70eae228098436d71
Habiba63 <enghabibahammouda@gmail.com>
2025-04-06 09:16:33 +0200

    /feed route initialized

--- apps/web/template/src/app/page.tsx
+++ apps/web/template/src/app/page.tsx
@@ -2,15 +2,11 @@
 
 import React from "react";
 import { Box } from "@mui/material";
-import Navbar from "./components/Navbar";
-import Feed from "./components/Feed";
 
 const HomePage: React.FC = () => {
   return (
     <Box sx={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center", mt: 2 }}>
-      <Navbar />
-      <Feed />
-
+      <p>Check out /feed</p>
     </Box>
   );
 };
