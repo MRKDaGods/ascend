5d132deea914fa29fcf9c7f5bb6e346ff02d4778
M7mdddibrahim <m7mdalhussini@gmail.com>
2025-04-11 21:08:17 +0200

    remember me and the signing in is WORKINGGG

--- apps/mobile/ascend_app/lib/features/StartPages/Presentation/Widget/InputWidgets.dart
+++ apps/mobile/ascend_app/lib/features/StartPages/Presentation/Widget/InputWidgets.dart
@@ -53,7 +53,7 @@ class InputValidators {
   }
 
   static String? validatePassword(String password) {
-    if (password.length < 6) {
+    if (password.length <= 3) {
       return 'Password should be at least 6 characters';
     }
     return null;
