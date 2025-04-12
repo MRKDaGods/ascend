87d7e25555809d2550356d32a163b9b9a362ee49
Habiba63 <enghabibahammouda@gmail.com>
2025-04-12 14:08:11 +0200

    pausing backend

--- apps/web/template/src/app/api/api.ts
+++ apps/web/template/src/app/api/api.ts
@@ -1,26 +1,25 @@
-// apps/web/template/src/app/api/api.ts
-import axios from "axios";
+// import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:3005",
-});
+// const API = axios.create({
+//   baseURL: "http://localhost:3005",
+// });
 
-// Optional: Replace this with your actual logic for getting the token
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQsImlhdCI6MTc0Mjk3MDkwOSwiZXhwIjoxNzQyOTc0NTA5fQ.rWr8KyRXn0wAAmuMm0qQ5tPpjtm8kw0DoqGclckM_1E";
+// // Optional: Replace this with your actual logic for getting the token
+// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQsImlhdCI6MTc0Mjk3MDkwOSwiZXhwIjoxNzQyOTc0NTA5fQ.rWr8KyRXn0wAAmuMm0qQ5tPpjtm8kw0DoqGclckM_1E";
 
-API.interceptors.request.use((config) => {
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQsImlhdCI6MTc0Mjk3MDkwOSwiZXhwIjoxNzQyOTc0NTA5fQ.rWr8KyRXn0wAAmuMm0qQ5tPpjtm8kw0DoqGclckM_1E";
+// API.interceptors.request.use((config) => {
+//     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQsImlhdCI6MTc0Mjk3MDkwOSwiZXhwIjoxNzQyOTc0NTA5fQ.rWr8KyRXn0wAAmuMm0qQ5tPpjtm8kw0DoqGclckM_1E";
   
-    // Ensure headers exist before modifying
-    if (config.headers) {
-      config.headers.Authorization = `Bearer ${token}`;
-    } else {
-      config.headers = {
-        Authorization: `Bearer ${token}`,
-      };
-    }
+//     // Ensure headers exist before modifying
+//     if (config.headers) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     } else {
+//       config.headers = {
+//         Authorization: `Bearer ${token}`,
+//       };
+//     }
   
-    return config;
-  });
+//     return config;
+//   });
 
-export default API;
+// export default API;
