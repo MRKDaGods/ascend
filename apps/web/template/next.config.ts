ce46d9222e2cb92d7717886df8aa9bb632a3790f
Habiba63 <enghabibahammouda@gmail.com>
2025-04-14 20:28:59 +0200

    notif and authen merged and running

--- apps/web/template/next.config.ts
+++ apps/web/template/next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: false,
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@ascend/api-client": path.resolve(__dirname, "../../../packages/api-client/pkg"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
