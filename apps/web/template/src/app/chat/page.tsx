e3f73f05ccd9dbcc2b879579f795a1035a4e20dd
Habiba63 <enghabibahammouda@gmail.com>
2025-04-15 02:19:11 +0200

    temp route to chat and profile

---
+++ apps/web/template/src/app/chat/page.tsx
@@ -0,0 +1,20 @@
+// import styles from "./page.module.css";
+// import Link from 'next/link';
+// import SidebarPreview from "./chat/components/SidebarPreview";
+
+
+
+// function Home() {
+//   return (
+//     <div className={styles.page}>
+//       <p>Integrate here, guyss!</p>
+//       <Link href="/chat">
+//                 <button>Go to Chat</button>
+//             </Link>
+//             {/* trying akenaha fel feed */}
+//             <SidebarPreview />
+//     </div>
+//   );
+// }
+
+// export default Home;
---
+++ apps/web/template/src/app/profile/page.tsx
@@ -0,0 +1,19 @@
+// 'use client';
+
+// import React from 'react';
+// import LinkedInProfile from './components/LinkedInProfile';
+// import styles from "./page.module.css";
+// import { ApiInitializer } from "../api";
+
+// function Home() {
+//   const renderContent = () => (<div className={styles.page}>
+//     <br></br>
+//     <LinkedInProfile />
+//   </div>);
+
+//   return (
+//     <ApiInitializer content={renderContent} />
+//   );
+// }
+
+// export default Home;
