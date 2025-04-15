7b72b6e9c51f12fc81be4ad71eb58fc34fa21bb0
Mohamed Ammar <mamar452@gmail.com>
2025-04-15 23:30:32 +0200

    Update apiDef.ts

--- apps/web/template/src/api/apiDef.ts
+++ apps/web/template/src/api/apiDef.ts
@@ -1,6 +1,6 @@
 import { ApiClient } from "@ascend/api-client/mrk";
 
 const API_URL_DEV = "http://127.0.0.1:8080";
-const API_URL_PROD = "http://api.ascendx.tech";
+const API_URL_PROD = "https://api.ascendx.tech";
 
 export const api = new ApiClient(API_URL_PROD);
