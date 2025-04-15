6508c411ec9500a9ea286906234254a2923821bb
Habiba63 <enghabibahammouda@gmail.com>
2025-04-15 14:41:37 +0200

    fixed ts error

--- apps/web/template/src/api/api.ts
+++ apps/web/template/src/api/api.ts
@@ -4,14 +4,16 @@ const API = axios.create({
   baseURL: "http://api.ascendx.tech/post",
 });
 
-// Static token for now 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNzQ0NjU2NjY2LCJleHAiOjE3NDQ2OTk4NjZ9.MUss-3OVisxAgxfZveo0PVthmN5ZFVRQg-SK7uc6QXE";
+// Static token for now
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwiaWF0IjoxNzQ0NjU2NjY2LCJleHAiOjE3NDQ2OTk4NjZ9.MUss-3OVisxAgxfZveo0PVthmN5ZFVRQg-SK7uc6QXE";
 
 API.interceptors.request.use((config) => {
-  config.headers = {
-    ...config.headers,
-    Authorization: `Bearer ${token}`,
-  };
+  // Ensure headers is an instance of AxiosHeaders
+  if (config.headers && typeof config.headers.set === "function") {
+    config.headers.set("Authorization", `Bearer ${token}`);
+  }
+
   return config;
 });
 
