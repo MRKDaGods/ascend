ccfaf050ac600e004f077c32140265826355ae39
Habiba63 <enghabibahammouda@gmail.com>
2025-04-16 12:43:42 +0200

    fixed errors

--- apps/web/template/src/app/layout.tsx
+++ apps/web/template/src/app/layout.tsx
@@ -1,25 +1,4 @@
-import React from "react";
-import "./globals.css";
-import { Geist, Geist_Mono } from "next/font/google";
-import Providers from "./providers/Provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Ascend",
-  description: "Software Engineering Project",
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
+import "../app/globals.css"; // Ensure the correct import path
 
 export default function RootLayout({
   children,
@@ -28,9 +7,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Providers>{children}</Providers>
-      </body>
+      <body className="bg-background text-foreground ">{children}</body>
+      
     </html>
   );
 }
--- apps/web/template/src/app/page.tsx
+++ apps/web/template/src/app/page.tsx
@@ -1,26 +1,35 @@
-import React from "react";
-import { Box, Container } from "@mui/material";
+"use client";
 
-const HomePage = () => {
+import { useEffect, useState } from "react";
+import { Box, Container, CircularProgress } from "@mui/material";
+
+export default function Home() {
+
+
   return (
-    <Box sx={{ bgcolor: "white", minHeight: "100vh" }}>
-      <Container>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: { xs: "column-reverse", md: "row" },
-            alignItems: "center",
-            justifyContent: "center",
-            gap: { xs: 2, md: 4 },
-            px: { xs: 2, md: 0 },
-            mt: { xs: 2, md: 0 }
-          }}
-        >
-          <p>Hello Nour! Here you can create Premium Plan Module. Good luck!</p>
-        </Box>
+    <Box
+  sx={{
+    minHeight: "100vh",
+    bgcolor: "grey.100",
+    display: "flex",
+    flexDirection: "column",
+  }}
+>
+      {/* Main Layout */}
+      <Container
+       sx={{
+        flexGrow: 1,
+        mt: 10,
+        display: "flex",
+        flexDirection: { xs: "column", md: "row" },
+        gap: 3,
+        maxWidth: "1200px",
+        pb: 3,
+      }}
+      >
+        <p>Hello Nour!</p>
       </Container>
+
     </Box>
   );
-};
-
-export default HomePage;
+}
