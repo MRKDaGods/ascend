1011e461c8867c347af58179ced3419eed7c17c7
Habiba63 <enghabibahammouda@gmail.com>
2025-04-26 01:06:43 +0300

    pausing profile

--- apps/web/template/src/app/components/ProfileCard.tsx
+++ apps/web/template/src/app/components/ProfileCard.tsx
@@ -20,7 +20,7 @@ const ProfileCard: React.FC = () => {
   const isLoading = !userData;
   const error = null;
 
-  const profileImg = userData?.profile_picture_url || "/default-avatar.jpg";
+  const profileImg = userData?.profile_picture_url || "/default-avatar.png";
   const coverImg = userData?.cover_photo_url || "/default-cover.png";
   const fullName = userData ? `${userData.first_name} ${userData.last_name}` : "";
   const isOpenToWork = true;
