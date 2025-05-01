3124f0a67346fe05be3405dd1112977eb9424c93
Mohamed Ayman <88878267+mohamed-ayman-28@users.noreply.github.com>
2025-05-01 15:04:26 +0300

    fixed two bugs in payment service

--- services/payment/src/services/surveyService.ts
+++ services/payment/src/services/surveyService.ts
@@ -2,7 +2,7 @@ import db from "@shared/config/db";
 import { Survey } from "@shared/models/survey";
 
 export const insertSurvey = async (user_id : number, question : string, answers : Array<string>, user_choice : number, date : Date) : Promise<Survey|null> => {
-    const result = await db.query("INSERT INTO payment_service.survey (user_id, question, answers, user_choice, date) VALUES ($1, $2, $3, $4, $5)", [user_id, question, answers, user_choice, date]);
+    const result = await db.query("INSERT INTO payment_service.survey (user_id, question, answers, user_choice, date) VALUES ($1, $2, $3, $4, $5) RETURNING *", [user_id, question, answers, user_choice, date]);
     return result.rows.length > 0 ? result.rows[0] : null;
 }
 
