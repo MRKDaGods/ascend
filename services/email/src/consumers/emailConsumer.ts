18e900b66334c2bf16a4b1e94db198f0f054d7eb
Mohamed Ammar <mamar452@gmail.com>
2025-05-04 20:42:40 +0300

    Update emailConsumer.ts

--- services/email/src/consumers/emailConsumer.ts
+++ services/email/src/consumers/emailConsumer.ts
@@ -3,8 +3,12 @@ import { getSystemUserEmail, sendEmail } from "../services/emailService";
 
 // Sends email using the system user
 export const handleSendEmail = async (payload: SendEmailPayload) => {
-    const sender = await getSystemUserEmail();
-    const { to, subject, content } = payload;
+  const sender = await getSystemUserEmail();
+  const { to, subject, content } = payload;
 
+  try {
     await sendEmail(sender, to, subject, content);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
 };
