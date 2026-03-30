f654bddb79200db2e641af6e497311a187fe962b
Mohamed Ammar <mamar452@gmail.com>
2026-03-30 18:49:57 +0200

    Remove redundant root jest/next configs and web deps from root package.json

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-};
